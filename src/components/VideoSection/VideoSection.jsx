fea82e5bcaf98aa307e3b2d06ed5871f592bfe5e
supreetkaur <supreet97@gmail.com>
2023-07-25 23:02:27 -0700

    comment form in app.jsx

--- src/components/VideoSection/VideoSection.jsx
+++ src/components/VideoSection/VideoSection.jsx
@@ -22,6 +22,11 @@ function VideoSection() {
                             <img className="video-section__icons" src={LikeIcon} alt="Like Icon" /> 110,985
                         </div>
                     </div>
+                    <div className="video-section__description">
+                    <p className="video-section__description-container">
+                        On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title
+                    </p>
+                    </div>
                 </div>
             </section>
         </>
