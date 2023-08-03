6a99771f04d350eefcb068ec4b6366d2f6700193
supreetkaur <supreet97@gmail.com>
2023-08-02 17:17:57 -0700

    random src creation - hero video re-render

--- src/components/HeroVideo/HeroVideo.jsx
+++ src/components/HeroVideo/HeroVideo.jsx
@@ -1,9 +1,13 @@
 import "./HeroVideo.scss";
 
 function HeroVideo({selectedVideoDetail}) {
+
+  const api_key = "?api_key=41a07f45-edd5-4710-950d-143b635e4bfc";
+  const randomNum = Math.random();
+
     return (
             <section className="hero-video">
-                <video className="hero-video__player" src={selectedVideoDetail.video} poster={selectedVideoDetail.image} controls></video>
+                <video className="hero-video__player" src={`${selectedVideoDetail.video}${api_key + randomNum}`} poster={selectedVideoDetail.image} controls></video>
             </section>
     );
 }
