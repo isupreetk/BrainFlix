74b9f09ae17ee64785b3de2afc018450f00b9582
Supreet Kaur <supreet97@gmail.com>
2023-10-06 11:08:22 -0700

    navigation for default page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
@@ -11,6 +11,7 @@ function App() {
       <Header />
 
       <Routes>
+        <Route path="/" element={<Navigate to="/videos" />} />
         <Route path="/videos" element={<HomePage />} />
         <Route path="/videos/:id" element={<HomePage />} />
         <Route path="/upload" element={<UploadPage />} />
