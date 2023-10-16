b4d5e64eee57d38955f6da354539f5158abaf9b7
Supreet Kaur <supreet97@gmail.com>
2023-10-16 16:44:06 -0700

    ignored .env file for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/videos" />} />
         <Route path="/videos" element={<HomePage />} />
-        {/* <Route path="/videos/:id" element={<HomePage />} /> */}
+        <Route path="/videos/:id" element={<HomePage />} />
         <Route path="/upload" element={<UploadPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
